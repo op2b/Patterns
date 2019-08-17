62e6c9f1ddd968e821dd96047e1b0ac1eb8de986
Artem <rsupanda@gmail.com>
2019-08-17 18:03:24 +0300

    observer commit 2 + second example

--- Observer.playground/Contents.swift
+++ Observer.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// FIRST PRIMER
 protocol SubJect {
     
     func add(observer: PropertyObserver)
@@ -57,3 +58,92 @@ let puipl = Pupil()
 teacher.add(observer: puipl)
 teacher.homeTask = "Выучить бородино"
 print(puipl.homeTask)
+
+
+
+// SECOND PRIMER
+protocol Observerebl {
+    func add(observer: Observer)
+    func remove(observer: Observer)
+    func notify()
+}
+
+protocol Observer {
+    var id: String {get set}
+    func update(value: Int?)
+}
+
+final class  NewsResourses: Observerebl {
+    
+    var value: Int? {
+        didSet {
+           notify()
+        }
+    }
+    
+    private var observers: [Observer] = []
+    
+    func add(observer: Observer) {
+        observers.append(observer)
+    }
+    
+    func remove(observer: Observer) {
+        guard let index = observers.enumerated().first(where: {$0.element.id == observer.id})?.offset else {return}
+        observers.remove(at: index)
+        
+    }
+    
+    func notify() {
+        observers.forEach {$0.update(value: value)}
+    }
+}
+
+final class NewsAgency: Observer {
+    var id = "news FIRST"
+    
+    func update(value: Int?) {
+        guard let value = value else {return}
+        print("NewAgancy handle update value \(value)")
+    }
+}
+
+final class Reporter: Observer {
+    var id = "reporter"
+    
+    func update(value: Int?) {
+        guard let value = value else {return}
+        print("Reporter. update value \(value)")
+    }
+}
+
+final class SofaCritic: Observer {
+    var id = "sofa critic"
+    
+    func update(value: Int?) {
+        guard let value = value else {return}
+        print("Sofa ctritic. New internet war about value: \(value)")
+    }
+}
+
+let resource = NewsResourses()
+let agancy = NewsAgency()
+let reporter = Reporter()
+let divanyCritic = SofaCritic()
+
+resource.add(observer: agancy)
+resource.add(observer: reporter)
+
+resource.value = 5
+
+resource.add(observer: divanyCritic)
+
+resource.value = 7
+
+resource.remove(observer: reporter)
+
+resource.value = 10
+
+
+
+
+
